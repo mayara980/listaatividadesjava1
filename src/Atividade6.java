01f8892c94f598e1099454ed5d06eee192680e2d
mayara980 <mayarafg_cvo@hotmail.com>
2025-10-02 21:26:29 -0300

    Criacao

---
+++ src/Atividade6.java
@@ -0,0 +1,10 @@
+public class Atividade6 {
+public static void main(String[] args) {
+
+    String nome = "Mayara";
+    String sobrenome = "Gomes de Mello";
+
+    System.out.println("Nome: "+nome+sobrenome);
+
+}}
+
--- src/Conversor.java
+++ src/Conversor.java
@@ -1,6 +1,7 @@
 public class Conversor {
     public static void main(String[] args) {
-           double valorDolar = 100.00;
-           double taxaCambio = 5.20;
-           double valorReais = valorDolar + taxaCambio
-           System.out.printl("Valor em dolares:$"+valorDolar)
+        double valorDolar = 100.00;
+        double taxaCambio = 5.20;
+        double valorReais = valorDolar + taxaCambio;
+        System.out.println("Valor em dolares:$" + valorDolar);
+    }}
